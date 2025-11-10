4e6504409ea6377b1414d56248e486e57937ca44
default <marco.cantusci@stu.ocsb.ca>
2025-11-10 11:48:03 +0000

    Finished A3

---
+++ A3.go
@@ -0,0 +1,55 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-10
+ * @fileoverview Display a multiplication table from 0 to 12 with the number 9.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// 0 x 9
+	fmt.Println("0 x 9 =", 0 * 9)
+
+	// 1 x 9
+	fmt.Println("1 x 9 =", 1 * 9)
+
+	// 2 x 9
+	fmt.Println("2 x 9 =", 2 * 9)
+
+	// 3 x 9
+	fmt.Println("3 x 9 =", 3 * 9)
+
+	// 4 x 9
+	fmt.Println("4 x 9 =", 4 * 9)
+
+	// 5 x 9
+	fmt.Println("5 x 9 =", 5 * 9)
+
+	// 6 x 9
+	fmt.Println("6 x 9 =", 6 * 9)
+
+	// 7 x 9
+	fmt.Println("7 x 9 =", 7 * 9)
+
+	// 8 x 9
+	fmt.Println("8 x 9 =", 8 * 9)
+
+	// 9 x 9
+	fmt.Println("9 x 9 =", 9 * 9)
+
+	// 10 x 9
+	fmt.Println("10 x 9 =", 10 * 9)
+
+	// 11 x 9
+	fmt.Println("11 x 9 =", 11 * 9)
+
+	// 12 x 9
+	fmt.Println("12 x 9 =", 12 * 9)
+
+	fmt.Println("\nDone.")
+
+}
---
+++ A3.ts
@@ -0,0 +1,47 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-10
+ * @fileoverview Display a multiplication table from 0 to 12 with the number 9.
+ */
+
+// 0 x 9
+console.log("0 x 9 =", 0 * 9);
+
+// 1 x 9
+console.log("1 x 9 =", 1 * 9);
+
+// 2 x 9
+console.log("2 x 9 =", 2 * 9);
+
+// 3 x 9
+console.log("3 x 9 =", 3 * 9);
+
+// 4 x 9
+console.log("4 x 9 =", 4 * 9);
+
+// 5 x 9
+console.log("5 x 9 =", 5 * 9);
+
+// 6 x 9
+console.log("6 x 9 =", 6 * 9);
+
+// 7 x 9
+console.log("7 x 9 =", 7 * 9);
+
+// 8 x 9
+console.log("8 x 9 =", 8 * 9);
+
+// 9 x 9
+console.log("9 x 9 =", 9 * 9);
+
+// 10 x 9
+console.log("10 x 9 =", 10 * 9);
+
+// 11 x 9
+console.log("11 x 9 =", 11 * 9);
+
+// 12 x 9
+console.log("12 x 9 =", 12 * 9);
+
+console.log("\nDone.");
